b6897316cc78782d3b4fa48b083cd3c3eb05e3f6
Sherin Mathew <skm20011@gmail.com>
2022-04-24 20:40:24 -0500

    added routes and handlers

--- backend-app/README.md
+++ backend-app/README.md
@@ -1,4 +1,8 @@
 # Go App
 
+# Install Go extension and Install/Update Tools(cmd + shift + R)
+
 ### go mod init backend
 ### go run cmd/api/main.go
+### go get -u github.com/julienschmidt/httprouter
+### go run cmd/api/*.go
--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -1,58 +1,58 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const version = "1.0.0"
 
-
 type config struct {
 	port int
-	env string
+	env  string
 }
 
 type AppStatus struct {
-	Status	string `json:"status"`
-	Environment	string `json:"environment"`
-	Version		string `json:"version"`
+	Status      string `json:"status"`
+	Environment string `json:"environment"`
+	Version     string `json:"version"`
 }
 
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
-	flag.StringVar(&cfg.env, "env", "dev", "App env (dev|prod)")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production")
 	flag.Parse()
 
-	fmt.Println("Running")
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		currentStatus := AppStatus {
-			Status: "Available",
-			Environment: cfg.env,
-			Version: version,
-		}
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
 
-		js, err := json.MarshalIndent(currentStatus, "", "\t")
-		if err != nil {
-			log.Println(err)
-		}
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(js)
-	})
+	logger.Println("Starting server on port", cfg.port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
-
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
-
 }
